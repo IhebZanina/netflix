124ecec7b7eda812f06233b663e205def2a12b68
Iheb zannina <iheb.zannina@gmail.com>
2024-03-11 23:03:01 +0100

    Building the Plans Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import PlansPage from "./pages/PlansPage";
 function App() {
   return (
     <div>
-      <LoginPage />
+      <PlansPage />
     </div>
   );
 }
